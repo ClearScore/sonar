afa6b912dfd999d2888c9250bee8549b26e9a4b9
Peter Mouland <peter.mouland@gmail.com>
2021-03-24 10:32:48 +0000

    fix(repo): polyfill int.listFormat for node v10

--- src/commands/lib/listify.js
+++ src/commands/lib/listify.js
@@ -1,3 +1,8 @@
+if (!('ListFormat' in Intl)) {
+    require('intl-list-format');
+    require('intl-list-format/locale-data/en');
+}
+
 function listify(array, { type = 'conjunction', style = 'long', stringify = (item) => item.toString() } = {}) {
     const stringified = array.map((item) => stringify(item));
     const formatter = new Intl.ListFormat('en', { style, type });
